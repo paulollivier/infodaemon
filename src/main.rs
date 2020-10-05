3e80bd36e9e104365e695d93aaaff22d0e16548b
Paul Ollivier <contact@paulollivier.fr>
2020-10-05 14:27:50 +0200

    forgot to change the bound ip to 0.0.0.0 u_u

--- src/main.rs
+++ src/main.rs
@@ -59,6 +59,6 @@ async fn main() {
         warp::reply::json(&info)
     });
     println!("Launching server...");
-    warp::serve(info).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(info).run(([0, 0, 0, 0], 3030)).await;
     println!("Quitting");
 }
