5853dc78dd064f1d0c737bb4fe32ee69c6c12da7
Paul Ollivier <contact@paulollivier.fr>
2020-10-05 11:26:43 +0200

    Add disks info to json output
    Also, fix docker build issue and re-use the debian image instead of
    alpine & musl

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,12 @@
 FROM rust:latest as builder
-RUN apt-get update
-RUN apt-get install musl-tools -y
-RUN rustup target add x86_64-unknown-linux-musl
-WORKDIR /usr/src/infodeamon
-COPY . .
-RUN RUSTFLAGS=-Clinker=musl-gcc cargo build --release --target=x86_64-unknown-linux-musl
+WORKDIR /usr/src/infodaemon
+COPY Cargo.toml Cargo.toml
+COPY src src
+RUN cargo install --path .
 
-FROM alpine:latest
-RUN addgroup -g 1000 infodaemon
-RUN adduser -D -s /bin/sh -u 1000 -G infodaemon infodaemon
-COPY --from=builder /usr/src/infodaemon/target/x86_64-unknown-linux-musl/release/infodaemon /usr/local/bin/infodaemon
-RUN chown infodaemon:infodaemon /usr/local/bin/infodaemon
-USER infodaemon
+FROM debian:buster-slim
+# this fails. Why?
+#RUN apt-get update && apt-get install -y extra-runtime-dependencies && rm -rf /var/lib/apt/lists/*
+COPY --from=builder /usr/local/cargo/bin/infodaemon /usr/local/bin/infodaemon
 EXPOSE 3030
 CMD ["infodaemon"]
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,48 @@ use std::env;
 
 use warp::Filter;
 
+use sysinfo::{DiskExt, System, SystemExt};
+
 use serde::Serialize;
+
+#[derive(Serialize)]
+struct DiskInfo {
+    name: String,
+    mount_point: String,
+    total_space: u64,
+    available_space: u64,
+}
 
 #[derive(Serialize)]
 struct SysInfo {
     env: HashMap<String, String>,
+    disks: Vec<DiskInfo>,
 }
 
 impl SysInfo {
     fn new() -> Self {
         let mut info = SysInfo {
             env: HashMap::new(),
+            disks: Vec::new(),
         };
+        let mut sysinfo = System::new_all();
+        sysinfo.refresh_all();
+        for disk in sysinfo.get_disks() {
+            info.disks.push(DiskInfo {
+                name: String::from(
+                    disk.get_name()
+                        .to_str()
+                        .expect("could not unwrap disk name to str"),
+                ),
+                mount_point: String::from(
+                    disk.get_mount_point()
+                        .to_str()
+                        .expect("could not unwrap disk mount point to str"),
+                ),
+                total_space: disk.get_total_space(),
+                available_space: disk.get_available_space(),
+            });
+        }
         for (k, v) in env::vars() {
             info.env.insert(k, v);
         }
